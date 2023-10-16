754e0efb8570719a3a000bb0038bfed9ace177d5
Ibraim Asanov <iasanov@nemetschek.bg>
2023-10-16 19:31:41 +0300

    webhook build

--- src/test/java/base/TestUtil.java
+++ src/test/java/base/TestUtil.java
@@ -69,5 +69,5 @@ public class TestUtil extends DataProviders {
         WebDriverManager.safaridriver().setup();
         return new SafariDriver();
     }
-    //web hook trigger build n
+    //web hook trigger build
 }
